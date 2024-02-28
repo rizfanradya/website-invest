f22f3a57a91e09e1e31270a64e092fb152338a00
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-02-28 11:56:37 +0000

    up

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,5 +1,16 @@
+"use client";
 import PageRouteSecure from "@/app/components/pageRouteSecure";
+import { signOut } from "next-auth/react";
 
 export default function Dashboard() {
-  return <PageRouteSecure>Dashboard</PageRouteSecure>;
+  return (
+    <PageRouteSecure>
+      <div className="flex items-center justify-center text-center flex-col h-screen gap-6">
+        <p className="font-bold text-4xl">DASHBOARD</p>
+        <button onClick={() => signOut()} className="btn btn-primary text-2xl">
+          LOGOUT
+        </button>
+      </div>
+    </PageRouteSecure>
+  );
 }
