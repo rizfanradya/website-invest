3121fb7af762a88476c58d57e0cb3625aaa00a42
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-02 18:04:11 +0700

    up auth

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -6,11 +6,12 @@ import LoginRegisterRouteSecure from "@/app/components/loginRegisterRouteSecure"
 export default function Register() {
   return (
     <LoginRegisterRouteSecure>
+      <title>Blibli - Register</title>
       <div className="flex justify-center items-center px-4 py-8">
         <div className="card max-w-sm bg-base-100">
           <div className="card-body">
             <div className="text-center flex flex-col items-center justify-center gap-2">
-              <Image alt="logo" width={75} height={75} src={"/logoimg.png"} />
+              <Image alt="logo" width={125} height={125} src={"/logo.png"} />
               <h1 className="font-semibold text-2xl mt-6">
                 Create Account Free
               </h1>
