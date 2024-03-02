210fc6181494e8c6002007456602cccdab1a3a53
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-02 10:02:15 +0700

    up

--- src/app/components/logout.tsx
+++ src/app/components/logout.tsx
@@ -1,10 +1,18 @@
 "use client";
-
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Logout() {
+  const router = useRouter();
   return (
-    <div className="btn btn-error text-white" onClick={() => signOut()}>
+    <div
+      className="btn btn-error text-white"
+      onClick={() => (
+        signOut({ redirect: false }),
+        router.push("/login"),
+        setTimeout(() => window.location.reload(), 500)
+      )}
+    >
       Logout
     </div>
   );
