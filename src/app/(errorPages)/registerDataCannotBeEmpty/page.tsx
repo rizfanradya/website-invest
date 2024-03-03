b6a0004c46526a771bbcff5f7bcf1939ee020c0b
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-04 01:59:32 +0700

    improve card

--- src/app/(errorPages)/registerDataCannotBeEmpty/page.tsx
+++ src/app/(errorPages)/registerDataCannotBeEmpty/page.tsx
@@ -1,29 +1,27 @@
 "use client";
-import { allowConsoleLog } from "@/utils/constant";
 import { useRouter } from "next/navigation";
-import Swal from "sweetalert2";
+import { VscError } from "react-icons/vsc";
 
 export default function RegisterDataCannotBeEmpty() {
   const router = useRouter();
-  const showAlert = async () => {
-    try {
-      const result = await Swal.fire({
-        icon: "error",
-        title: "Error",
-        text: "Data Cannot Be Empty",
-        showCancelButton: false,
-        confirmButtonText: "OK",
-        allowOutsideClick: false,
-      });
 
-      if (result.isConfirmed) {
-        router.push("/register");
-      }
-    } catch (error) {
-      allowConsoleLog && console.log(error);
-      router.push("/register");
-    }
-  };
-  showAlert();
-  return <title>Blibli - Data Cannot Be Empty</title>;
+  return (
+    <div className="flex items-center justify-center w-screen h-screen p-4">
+      <title>Blibli - Data Cannot Be Empty</title>
+
+      <div className="card bg-white shadow-xl text-slate-600 w-full max-w-md py-6">
+        <div className="card-body flex flex-col items-center justify-center gap-4 py-4">
+          <VscError size={100} className="text-error" />
+          <h1 className="text-4xl font-semibold">Error</h1>
+          <p className="py-4 text-2xl">Data Cannot Be Empty</p>
+          <div
+            className="btn btn-primary text-white text-lg"
+            onClick={() => router.push("/register")}
+          >
+            OK
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/(errorPages)/resetPasswordUserNotFound/page.tsx
+++ src/app/(errorPages)/resetPasswordUserNotFound/page.tsx
@@ -1,29 +1,27 @@
 "use client";
-import { allowConsoleLog } from "@/utils/constant";
 import { useRouter } from "next/navigation";
-import Swal from "sweetalert2";
+import { VscError } from "react-icons/vsc";
 
 export default function ResetPasswordUserNotFound() {
   const router = useRouter();
-  const showAlert = async () => {
-    try {
-      const result = await Swal.fire({
-        icon: "error",
-        title: "Error",
-        text: "User Not Found",
-        showCancelButton: false,
-        confirmButtonText: "OK",
-        allowOutsideClick: false,
-      });
 
-      if (result.isConfirmed) {
-        router.push("/resetPassword");
-      }
-    } catch (error) {
-      allowConsoleLog && console.log(error);
-      router.push("/resetPassword");
-    }
-  };
-  showAlert();
-  return <title>Blibli - User Not Found</title>;
+  return (
+    <div className="flex items-center justify-center w-screen h-screen p-4">
+      <title>Blibli - User Not Found</title>
+
+      <div className="card bg-white shadow-xl text-slate-600 w-full max-w-md py-6">
+        <div className="card-body flex flex-col items-center justify-center gap-4 py-4">
+          <VscError size={100} className="text-error" />
+          <h1 className="text-4xl font-semibold">Error</h1>
+          <p className="py-4 text-2xl">User Not Found</p>
+          <div
+            className="btn btn-primary text-white text-lg"
+            onClick={() => router.push("/resetPassword")}
+          >
+            OK
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
