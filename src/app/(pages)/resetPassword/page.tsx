5bdda52bcd6ada87f4362b91709acc22da1bedb4
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-13 22:45:47 +0700

    up

--- src/app/(pages)/resetPassword/page.tsx
+++ src/app/(pages)/resetPassword/page.tsx
@@ -1,4 +1,3 @@
-import LoginRegisterRouteSecure from "@/app/components/loginRegisterRouteSecure";
 import Image from "next/image";
 import Link from "next/link";
 import prisma from "@/utils/db";
@@ -7,8 +6,14 @@ import { FaKey } from "react-icons/fa6";
 import bcrypt from "bcrypt";
 import { redirect } from "next/navigation";
 import FormButton from "@/app/components/formButton";
+import { getServerSession } from "next-auth";
 
-export default function ResetPassword() {
+export default async function ResetPassword() {
+  const session: any = await getServerSession();
+  if (session) {
+    redirect("/");
+  }
+
   async function onSubmit(formData: FormData) {
     "use server";
     async function handleSubmit() {
@@ -34,57 +39,51 @@ export default function ResetPassword() {
   }
 
   return (
-    <LoginRegisterRouteSecure>
+    <div className="flex justify-center items-center px-4 py-8">
       <title>Blibli - Reset Password</title>
+      <div className="card max-w-sm bg-base-100">
+        <div className="card-body">
+          <div className="text-center flex flex-col items-center justify-center gap-2">
+            <Image alt="logo" width={125} height={125} src={"/logo.png"} />
+            <h1 className="font-semibold text-2xl mt-6">Reset Password</h1>
 
-      <div className="flex justify-center items-center px-4 py-8">
-        <div className="card max-w-sm bg-base-100">
-          <div className="card-body">
-            <div className="text-center flex flex-col items-center justify-center gap-2">
-              <Image alt="logo" width={125} height={125} src={"/logo.png"} />
-              <h1 className="font-semibold text-2xl mt-6">Reset Password</h1>
+            <form
+              className="text-start w-full mt-8 flex flex-col gap-4"
+              action={onSubmit}
+            >
+              <div className="flex items-center justify-between input input-bordered rounded-2xl">
+                <input
+                  id="Username"
+                  type="text"
+                  placeholder="Username"
+                  className="w-full"
+                  required
+                  name="username"
+                />
+                <FaUserAlt size={"1.5em"} className="text-slate-500" />
+              </div>
 
-              <form
-                className="text-start w-full mt-8 flex flex-col gap-4"
-                action={onSubmit}
-              >
-                <div className="flex items-center justify-between input input-bordered rounded-2xl">
-                  <input
-                    id="Username"
-                    type="text"
-                    placeholder="Username"
-                    className="w-full"
-                    required
-                    name="username"
-                  />
-                  <FaUserAlt size={"1.5em"} className="text-slate-500" />
-                </div>
+              <div className="flex items-center justify-between input input-bordered rounded-2xl">
+                <input
+                  id="Password"
+                  type="password"
+                  placeholder="New Password"
+                  className="w-full"
+                  required
+                  name="newPassword"
+                />
+                <FaKey size={"1.5em"} className="text-slate-500" />
+              </div>
 
-                <div className="flex items-center justify-between input input-bordered rounded-2xl">
-                  <input
-                    id="Password"
-                    type="password"
-                    placeholder="New Password"
-                    className="w-full"
-                    required
-                    name="newPassword"
-                  />
-                  <FaKey size={"1.5em"} className="text-slate-500" />
-                </div>
+              <Link href={"/login"} className="text-primary font-semibold mb-8">
+                Login
+              </Link>
 
-                <Link
-                  href={"/login"}
-                  className="text-primary font-semibold mb-8"
-                >
-                  Login
-                </Link>
-
-                <FormButton text="Reset" />
-              </form>
-            </div>
+              <FormButton text="Reset" />
+            </form>
           </div>
         </div>
       </div>
-    </LoginRegisterRouteSecure>
+    </div>
   );
 }
