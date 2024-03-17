6c3d9076039d38618eb2efb2a9f068330ce6f8c4
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-17 22:40:55 +0700

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: { serverActions: { allowedOrigins: ["blibli71.com"] } },
+};
 
 export default nextConfig;
